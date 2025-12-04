068fcb4cd5cb71e9700bdea72311388be106a69d
jonathanmoralesmurillo-netizen <jonathan.morales.murillo@una.cr>
2025-12-04 15:11:00 -0600

    Initial commit

---
+++ utiles.cpp
@@ -0,0 +1,51 @@
+//
+// Created by ESINF 2-004805 on 4/12/25.
+//
+//
+// Created by ESINF 2-004805 on 4/12/25.
+//
+
+#ifndef CLASE01RU_UTILES_H
+#define CLASE01RU_UTILES_H
+
+
+#include "utiles.h"
+
+void imprimeCadena(string cadena) {
+    cout << cadena;
+}
+
+string leerCadena() {
+    string x;
+    getline(cin, x);
+    return x;
+}
+
+int leerEntero() {
+    int n;
+    bool continuar = true;
+    while (true) {
+        if (cin >> n) {
+            cin.clear(); // Limpia el estado del flujo..
+            cin.ignore(1024, '\n');
+            return n;
+        }
+        else {
+            cerr << "Valor incorrecto.. digita UN NUMERO" << endl;
+            cin.clear();
+            cin.ignore(1024, '\n');
+        }
+    }
+
+}
+
+void limpiaPantalla(){
+    system("cls");
+}
+
+void esperandoEnter() {
+    cin.get();
+}
+
+
+#endif //CLASE01RU_UTILES_H
---
+++ utiles.h
@@ -0,0 +1,14 @@
+#ifndef UTILES_H
+#define UTILES_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void imprimeCadena(string);
+void limpiaPantalla();
+string leerCadena();
+int leerEntero();
+void esperandoEnter();
+
+#endif
